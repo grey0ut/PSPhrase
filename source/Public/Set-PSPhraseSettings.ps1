a92cd36ea0be2310251c3b3cfd7a52f287ddc6e6
Courtney Bodett <courtney.bodett@gmail.com>
2025-03-10 21:12:26 -0700

    updated help to include parameters

--- source/Public/Set-PSPhraseSettings.ps1
+++ source/Public/Set-PSPhraseSettings.ps1
@@ -4,8 +4,26 @@ function Set-PSPhraseSettings {
     Configures the parameter defaults for New-PSPhrase by saving the settings to a file
     .DESCRIPTION
     Default parameter values for New-PSPhrase are retrieved via Get-PSPhraseSettings.  To reduce the amount of parameters you have to pass to New-PSPhrase you can set your preferences
-    with Set-PSPhraseSettings and they will be saved to a file for retrieval by Get-PSPhraseSettings by default later
-    
+    with Set-PSPhraseSettings and they will be saved to a file for retrieval by Get-PSPhraseSettings by default later.  This was created as a more accessible way to achieve default
+    parameter values without using $PSDefaultParameterValues and the $Profile
+    .PARAMETER Pairs
+    It generates Adjective/Noun pairs, this defines how many pairs you want returned. The default is 2 pair (two words)
+    .PARAMETER TitleCase
+    Switch parameter to toggle the use of title case. E.g. The First Letter Of Every Word Is Uppercase. 
+    .PARAMETER Substitution
+    Switch parameter to toggle common character substitution.  E.g. '3's for 'e's and '@'s for 'a's etc. 
+    .PARAMETER Prepend
+    Provide a string you would like prepended to the password output
+    .PARAMETER Append
+    Provide a string you would like appended to the password output
+    .PARAMETER Delimiter
+    This parameter accepts any string you would prefer as a delimiter between words. Defaults to a space.
+    .PARAMETER IncludeNumber
+    Switch parameter to randomly include a number within the passphrase
+    .PARAMETER IncludeSymbol
+    Switch parameter to randomly include a symbol within the passphrase
+    .PARAMETER Defaults
+    Reverts settings to default state by deleting the saved settings file and allowing New-PSPhrase to use default parameter values
     #>
     [CmdletBinding()]
     param (
