4ecd4409767e901bf08f8fa54781aafa6f57154b
Courtney Bodett <bode235@lni.wa.gov>
2025-03-12 16:16:20 -0700

    updated to satisfy pester tests

--- source/Private/Get-RandomInt.ps1
+++ source/Private/Get-RandomInt.ps1
@@ -1,4 +1,19 @@
 Function Get-RandomInt {
+    <#
+    .SYNOPSIS
+    More robust method of getting a random number than Get-Random
+    .DESCRIPTION
+    Leverages the .NET RNGCryptoServiceProvider to retrieve a random number
+    .PARAMETER Minimum
+    Minimum number for range of random number generation
+    .PARAMETER Maximum
+    Maximum number for range of random number generation
+    .EXAMPLE
+    PS> Get-RanomInt -Minimum 1 -Maximum 1000
+    838
+
+    will return a random number from between 1 and 1000
+    #>
     param (
         [UInt32]$Minimum,
         [UInt32]$Maximum
--- source/Private/Initialize-Dictionary.ps1
+++ source/Private/Initialize-Dictionary.ps1
@@ -1,5 +1,19 @@
 function Initialize-Dictionary {
+    <#
+    .SYNOPSIS
+    Creates ordered hashtables of wordlists for faster retrieval with random number generator
+    .DESCRIPTION
+    Reads in a text file containing one word per line and creates an ordered dictionary starting the keys with '1' and incrementing up from there
+    for each word added.  Then using a RNG a corresponding key can be called and the associated value (word) can be retrieved very quickly
+    .PARAMETER Type
+    Whether to load the Nouns or Adjectives list
+    .EXAMPLE
+    $Nouns = Initialize-Dictionary -Type Nouns
+
+    will turn $Nouns in to a hashtable containing all the words from the Nouns.txt file
+    #>
     [Cmdletbinding()]
+    [OutputType([System.Collections.Specialized.OrderedDictionary])]
     param (
         [ValidateSet("Nouns","Adjectives")]
         [String]$Type
